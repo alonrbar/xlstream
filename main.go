b8f381cb6b2d195bf4ef29db0c31514ab04800fd
alonrbar <alon.r.bar@gmail.com>
2022-11-14 23:51:22 +0200

    Concept

--- main.go
+++ main.go
@@ -1,7 +1,68 @@
 package main
 
-import "log"
+import (
+	"archive/zip"
+	"bufio"
+	"encoding/xml"
+	"fmt"
+	"log"
+	"strings"
+)
 
 func main() {
-	log.Print("Hello world")
+	fmt.Print("Hello")
+
+	sheet, err := openFirstSheet("test.xlsx")
+	if err != nil {
+		log.Fatalf("openFirstSheet failed: %v", err)
+	}
+
+	readRows(sheet)
+
+	fmt.Print("Bye")
+}
+
+func openFirstSheet(src string) (*zip.File, error) {
+	zipReader, err := zip.OpenReader(src)
+	if err != nil {
+		return nil, err
+	}
+	defer zipReader.Close()
+
+	for _, f := range zipReader.File {
+		if strings.HasSuffix(f.Name, "/sheet1.xml") {
+			return f, nil
+		}
+	}
+
+	return nil, fmt.Errorf("first sheet not found")
+}
+
+func readRows(sheet *zip.File) {
+	reader, err := sheet.Open()
+	if err != nil {
+		log.Fatalf("Failed to open sheet: %v", err)
+	}
+	defer reader.Close()
+
+	_ = xml.NewDecoder(reader)
+	_ = bufio.NewReader(reader)
+
+	for {
+		readRow()
+	}
+}
+
+func readRow() {
+	// find row start element
+
+	// for
+	// 	find next element
+	// 	if start column
+	// 		read value
+	// 		continue
+	//	if end row
+	//		return
+	//  if end of file
+	//  	return
 }
